2eaa99f96b5a05c76c31c078ab564450526afa01
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-09-15 20:40:36 +0900

    Add tests

---
+++ tests/test_attention.py
@@ -0,0 +1,48 @@
+import torch
+from torch.autograd import Variable
+from torch import nn
+from onmt.modules import GlobalAttention
+
+import sys
+sys.path.append("..")
+
+from tacotron_pytorch.attention import BahdanauAttention, AttentionWrapper
+from tacotron_pytorch.attention import get_mask_from_lengths
+
+
+def test_attention_wrapper():
+    B = 2
+
+    encoder_outputs = Variable(torch.rand(B, 100, 256))
+    memory_lengths = [100, 50]
+
+    mask = get_mask_from_lengths(encoder_outputs, memory_lengths)
+    print("Mask size:", mask.size())
+
+    memory_layer = nn.Linear(256, 256)
+    query = Variable(torch.rand(B, 128))
+
+    attention_mechanism = BahdanauAttention(256)
+
+    # Attention context + input
+    rnn = nn.GRUCell(256 + 128, 256)
+
+    attention_rnn = AttentionWrapper(rnn, attention_mechanism)
+    initial_attention = Variable(torch.zeros(B, 256))
+    cell_state = Variable(torch.zeros(B, 256))
+
+    processed_memory = memory_layer(encoder_outputs)
+
+    cell_output, attention, alignment = attention_rnn(
+        query, initial_attention, cell_state, encoder_outputs,
+        processed_memory=processed_memory,
+        mask=None, memory_lengths=memory_lengths)
+
+    print("Cell output size:", cell_output.size())
+    print("Attention output size:", attention.size())
+    print("Alignment size:", alignment.size())
+
+    assert (alignment.sum(-1) == 1).data.all()
+
+
+test_attention_wrapper()
---
+++ tests/test_tacotron.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import sys
+sys.path.append("./lib/tacotron")
+from text import text_to_sequence, symbols
+import torch
+from torch.autograd import Variable
+from tacotron_pytorch import Tacotron
+import numpy as np
+
+
+def _pad(seq, max_len):
+    return np.pad(seq, (0, max_len - len(seq)),
+                  mode='constant', constant_values=0)
+
+
+def test_taco():
+    B, T_out, D_out = 2, 400, 80
+    r = 5
+    T_encoder = T_out // r
+
+    texts = ["Thank you very much.", "Hello"]
+    seqs = [np.array(text_to_sequence(
+        t, ["english_cleaners"]), dtype=np.int) for t in texts]
+    input_lengths = np.array([len(s) for s in seqs])
+    max_len = np.max(input_lengths)
+    seqs = np.array([_pad(s, max_len) for s in seqs])
+
+    x = torch.LongTensor(seqs)
+    y = torch.rand(B, T_out, D_out)
+    x = Variable(x)
+    y = Variable(y)
+
+    model = Tacotron(n_vocab=len(symbols), r=r)
+
+    print("Encoder input shape: ", x.size())
+    print("Decoder input shape: ", y.size())
+    a, b, c = model(x, y, input_lengths=input_lengths)
+    print("Mel shape:", a.size())
+    print("Linear shape:", b.size())
+    print("Attention shape:", c.size())
+
+    assert c.size() == (B, T_encoder, max_len)
+
+    # Test greddy decoding
+    a, b, c = model(x, input_lengths=input_lengths)
